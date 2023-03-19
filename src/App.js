95218ad262567ad1d06a6a4b33b418a138a69ee0
Roman <alterede@yandex.ru>
2023-03-19 22:49:31 +0300

    ad

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import Footer from "./components/Footer";
 import './App.css';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="do" element={<Footer />} />
+    </Routes>
+  )
 }
 
 export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ThreeDivs() {
+  return (
+    <div>
+      <div>4 блок</div>
+      <div>5 блок</div>
+      <div>6 блок</div>
+    </div>
+  );
+}
+
+export default ThreeDivs;
